260cbf1979ed6943037dd1b097436759e2abe654
ra5922 <ridhimaagrawal8739@gmail.com>
2026-02-23 10:21:45 +0530

    UC4 completed

--- PalindromeCheckerApps/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApps/src/PalindromeCheckerApp.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
-        String original = sc.nextLine();
+        String input = sc.nextLine();
 
-        String reversed = "";
+        char[] charArray = input.toCharArray();
 
+        int start = 0;
+        int end = charArray.length - 1;
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
-        if (original.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("It is a palindrome.");
         } else {
             System.out.println("It is not a palindrome.");
